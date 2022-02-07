a4a6472b1275718042543f5f129de3769840ca72
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-02-07 10:40:18 +0700

    initial project

---
+++ NewsApp_2UITests/NewsApp_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NewsApp_2UITestsLaunchTests.swift
+//  NewsApp_2UITests
+//
+//  Created by Siroratt Suntronsuk on 7/2/2565 BE.
+//
+
+import XCTest
+
+class NewsApp_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
