7a11cf73e5a1ffaae4e0afb347c803c3d26f12ea
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-02-14 12:11:40 +0700

    Day3 - calendar button

--- NewsApp_2/NewsVC.swift
+++ NewsApp_2/NewsVC.swift
@@ -6,8 +6,18 @@
 //
 
 import UIKit
+import EventKit
+import EventKitUI
 
+
+extension NewsVC: EKEventViewDelegate {
+    func eventViewController(_ controller: EKEventViewController, didCompleteWith action: EKEventViewAction) {
+        dismiss(animated: true, completion: nil)
+    }
+}
 class NewsVC: UIViewController {
+    
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bodyTextView: UITextView!
     @IBOutlet weak var calenderButton: UIButton!
@@ -20,8 +30,29 @@ class NewsVC: UIViewController {
         bodyTextView.isUserInteractionEnabled = false
     }
     
+    let calendarStore = EKEventStore()
     @objc func calendarButtonDidClick(_ sender: Any) {
-        
+        calendarStore.requestAccess(to: .event,
+                                    completion: xxxx)
+    }
+    
+    func xxxx(status: Bool, error: Error?) {
+        if status, error == nil {
+            
+    
+            let newEvent = EKEvent(eventStore: self.calendarStore)
+            newEvent.title = "AnnJa event"
+            newEvent.startDate = Date()
+            newEvent.endDate = Date()
+            
+            DispatchQueue.main.async {
+                let vc = EKEventViewController()
+                vc.event = newEvent
+                vc.delegate = self
+                let navVC = UINavigationController(rootViewController: vc)
+                self.present(navVC, animated: true, completion: nil)
+            }
+        }
     }
     
     @IBAction func forMoreDetailDidClick(_ sender: Any) {
